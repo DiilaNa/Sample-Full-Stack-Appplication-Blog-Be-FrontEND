ea218e18757a059b27668f826a8908ffb89e1e1e
DiilaNa <liyanaarachchidilan@gmail.com>
2025-11-07 11:34:36 +0530

    Add auth ts

---
+++ src/services/auth.ts
@@ -0,0 +1,19 @@
+import api from "./api";
+
+type RegistrationDataType = {
+    firstname:string
+    lastname:string
+    email:string
+    password:string
+    confirmpassword:string
+}
+
+export const registration = async(data: RegistrationDataType) => {
+    const res = await api.post("/auth/register",data)
+    return  res.data
+}
+
+export const login = async(email:string, password:string){
+    const res = await api.post("/auth/login",{email,password})
+    return res.data
+}
